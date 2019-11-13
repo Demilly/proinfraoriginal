e65cca61d270d531ce9f23d3b144bf9c8b325bc2
demilly <demilly_@hotmail.com>
2019-11-13 02:05:07 -0300

    #0001-MAS-demillyfelipe-commit-inicial-criacao-da-api

---
+++ src/main/java/com/ApiProInfra/ApiProInfraApplication.java
@@ -0,0 +1,13 @@
+package com.ApiProInfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProInfraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiProInfra/ApiProInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiProInfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
